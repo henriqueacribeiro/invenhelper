50c0c3263bef3d27e79b0e2171e7a543be2e0c0f
Henrique Ribeiro <henriqueacribeiro@outlook.pt>
2021-07-15 19:56:45 +0100

    [US] [closes #8] Added SQL script creation

--- .configfile/databaseinit.sql
+++ .configfile/databaseinit.sql
@@ -10,3 +10,18 @@ CREATE TABLE `product` (
    `quantity` INT(10) NOT NULL DEFAULT '0',
    PRIMARY KEY (`id`) USING BTREE
 ) COLLATE='utf8mb4_0900_ai_ci' ENGINE=InnoDB;
+
+CREATE TABLE `user`
+(
+    `id`                   VARCHAR(255) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+    `username`             VARCHAR(255) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+    `name`                 VARCHAR(255) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+    `can_modify_inventory` INT(10) NOT NULL DEFAULT '0' COMMENT '0 if false, 1 if true',
+    `can_modify_products`  INT(10) NOT NULL DEFAULT '0' COMMENT '0 if false, 1 if true',
+    `can_add_users`        INT(10) NOT NULL DEFAULT '0' COMMENT '0 if false, 1 if true',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB
+;
+
+INSERT INTO user(id, username, name, can_modify_inventory, can_modify_products, can_add_users) VALUES ('30559387-ef67-45f9-b4a6-8f5f799955ad', 'admin', 'Administrador', 1, 1, 1)
+;
--- src/test/resources/databaseinit.sql
+++ src/test/resources/databaseinit.sql
@@ -1,5 +1,7 @@
 SET MODE MYSQL;
 DROP TABLE IF EXISTS `product`;
+DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `product` (
 	`id` VARCHAR(255) NOT NULL,
 	`business_id` TEXT NOT NULL ,
@@ -7,4 +9,17 @@ CREATE TABLE `product` (
 	`description` TEXT NOT NULL ,
 	`quantity` INT(10) NOT NULL DEFAULT '0',
 	PRIMARY KEY (`id`)
-)
+);
+
+CREATE TABLE `user`
+(
+    `id`                   VARCHAR(255) NOT NULL,
+    `username`             VARCHAR(255) NOT NULL,
+    `name`                 VARCHAR(255) NOT NULL,
+    `can_modify_inventory` INT(10) NOT NULL DEFAULT '0' COMMENT '0 if false, 1 if true',
+    `can_modify_products`  INT(10) NOT NULL DEFAULT '0' COMMENT '0 if false, 1 if true',
+    `can_add_users`        INT(10) NOT NULL DEFAULT '0' COMMENT '0 if false, 1 if true',
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO user(id, username, name, can_modify_inventory, can_modify_products, can_add_users) VALUES ('30559387-ef67-45f9-b4a6-8f5f799955ad', 'admin', 'Administrador', 1, 1, 1);

eef6e5dd0021bd9018d2eb164939ced50784bd9b
jtrodrigues <joaotomas_rodrigues@outlook.com>
2022-10-17 15:45:47 +0100

    [Basic UI] [refs #13] Increase and decrease product quantity. Alert correction.

--- user-interface-invenhelper/src/environments/environment.prod.ts
+++ user-interface-invenhelper/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  backend: {
+    host: "http://localhost:8080/",
+    getProducts: "product/getAllIdentifiers",
+    getProductByID: "product/getByID",
+    increaseQuantity: "product/increaseQuantity",
+    decreaseQuantity: "product/decreaseQuantity",
+  }
 };
